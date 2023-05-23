85bf6ac8a37c7d48670afd5cba4ad303b76d0e2b
liza <123323177+younggengaar@users.noreply.github.com>
2023-05-23 11:19:03 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //n12
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write($"e={x % 10}\td={x / 10}");
+            //Console.ReadKey();
+
+            //n13
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if(x>0)
+            //{
+            //    Console.WriteLine($"{x + 1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{x-2}");
+            //}
+            //Console.ReadKey();
+
+            //n14
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if(x>0)
+            //{
+            //    Console.WriteLine($"{x + 1}");
+            //}
+            //else if(x<0)
+            //{
+            //    Console.WriteLine($"{x-2}");
+            //}
+            //else(x==0)
+            //{
+            //    Console.WriteLine(10);
+            //}
+            //Console.ReadKey();
+
+            //n15
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if(x>0)
+            //{
+            //    Console.WriteLine($"Число{x}-положительное");
+            //}
+            //if(x<0)
+            //{
+
+            //}
+
+            //n16
+            //Console.Write("a=");
+            //    int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //    int b = int.Parse(Console.ReadLine());
+            //Console.Write("c=");
+            //    int c = int.Parse(Console.ReadLine());
+            //if(a==b&&b==c)
+            //{
+            //    Console.WriteLine("Треугольник равносторонний");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Треугольник не равносторонний");
+            //}
+            //Console.ReadKey();
+
+            //n17
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if(3%x==0)
+            //{
+            //    Console.WriteLine($"Число{x}является делителем 3");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число{x}не является делителем 3");
+            //}
+            //Console.ReadKey();
+
+            //n18
+            //Random rnd = new Random();//создается объект для генерации рандомных чисел
+            //for(int x=1;x<=10;x++)
+            //{
+            //    int value = rnd.Next(-30, 30);
+            //    if (value > 0)
+            //        Console.WriteLine($"{value}-положительное");
+            //    else
+            //        Console.WriteLine($"{value}-отрицательное");
+            //}
+            //Console.ReadKey();
+
+            //n19
+            //Random rnd = new Random();
+            //for (int x = 1; x <= 8; x++)
+            //{
+            //    int value = rnd.Next(-20, 20);
+            //    if (value%2==0)
+            //        Console.WriteLine($"{value}-четное");
+            //    else
+            //        Console.WriteLine($"{value}-нечетное");
+            //}
+            //Console.ReadKey();
+
+            //n20
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //if(x>a)
+            //{
+            //    Console.WriteLine($"{x}-max");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{a}-max");
+            //}
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
